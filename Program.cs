eede3a92012cce179b371fe57ffa8da8a6050129
Sean <sean081694@gmail.com>
2017-11-15 15:18:12 -0700

    Exercise
    Done the number thing

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 100;
+            int i = 1;
+
+            while (i <= Number) {
+                if(i % 15 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                if(i % 3 == 0 ){
+                    Console.WriteLine("Fizz");
+                }
+                if(i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else {
+                Console.WriteLine(i);
+                }
+                i++;
+                }
+        }
+    }
+}
