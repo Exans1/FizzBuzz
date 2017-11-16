b5e0d4ce33dbee868462347e6138104a5e275162
Sean <sean081694@gmail.com>
2017-11-15 17:14:16 -0700

    Made Changes
    Notes to do same things

--- Program.cs
+++ Program.cs
@@ -10,18 +10,19 @@ namespace FizzBuzz
             int i = 1;
 
             while (i <= Number) {
-                if(i % 3 == 0 ){
+                Console.WriteLine(i % 3 == 0 && i % 5 == 0 /*i % 15 == 0*/? "FizzBuzz": (i % 3 == 0 ? " Fizz": (i % 5 == 0 ? " Buzz":(i + ""))));
+                /*if (i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0 ){
                     Console.WriteLine("Fizz");
                 }
                 else if(i % 5 == 0){
                     Console.WriteLine("Buzz");
                 }
-                else if(i % 15 == 0){
-                    Console.WriteLine("FizzBuzz");
-                }
                 else {
                 Console.WriteLine(i);
-                }
+                }*/
                 i++;
                 }
         }
